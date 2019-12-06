a81d6c0d5a762c0d084a3c7f8d7ff6e528b4f717
Yeroshevich Valeriy <pybin2001@gmail.com>
2019-12-06 13:42:26 +0300

    jd15

--- src/by/it/zimina/jd01_15/TaskA.java
+++ src/by/it/zimina/jd01_15/TaskA.java
@@ -5,33 +5,32 @@ import java.util.Arrays;
 
 public class TaskA {
 
-    private static String dir(Class<?> cl){
-        String path=System.getProperty("user.dir")+ File.separator+"src"+File.separator;
-        String clDir=cl.getName().replace(cl.getSimpleName(),"").replace(".",File.separator);
-        return path+clDir;
+    private static String dir(Class<?> cl) {
+        String path = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        String clDir = cl.getName().replace(cl.getSimpleName(), "").replace(".", File.separator);
+        return path + clDir;
     }
 
-    private static int[][] generateMatrix(){
-        int matrix[][]= new int[6][4];
+    private static int[][] generateMatrix() {
+        int matrix[][] = new int[6][4];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j]=(int)(Math.random() * 30 - 15);
+                matrix[i][j] = (int) (Math.random() * 30 - 15);
             }
         }
         return matrix;
     }
 
-    private static void writeMatrix(int[][] matrix){
+    private static void writeMatrix(int[][] matrix) {
         try {
-            PrintWriter printWriter=new PrintWriter(dir(TaskA.class) + "matrix.txt");
+            PrintWriter printWriter = new PrintWriter(dir(TaskA.class) + "matrix.txt");
+
             for (int[] row : matrix) {
-          /*      printWriter.println(Arrays.toString(row)
-                        .replace(",","")
-                        .replace("[","")
-                        .replace("]","")); */
-          printWriter.printf("%3s \n", Arrays.toString(row)
-                  .replace("[","")
-                  .replace("]",""));
+
+                for (int i : row) {
+                    printWriter.printf("%-3s ", i);
+                }
+                printWriter.println();
             }
             printWriter.close();
         } catch (IOException e) {
@@ -43,19 +42,14 @@ public class TaskA {
     public static void main(String[] args) {
         int[][] matrix = generateMatrix();
         for (int[] row : matrix) {
-            /* System.out.println(Arrays.toString(row)
-                    .replace(",","")
-                    .replace("[","")
-                    .replace("]","")); */
-            System.out.printf("%3s \n", Arrays.toString(row)
-                    .replace("[","")
-                    .replace("]",""));
+            for (int i : row) {
+                System.out.printf("%-3s ", i);
+            }
+            System.out.println();
         }
         writeMatrix(matrix);
 
-        }
-
-
+    }
 
 
 }
