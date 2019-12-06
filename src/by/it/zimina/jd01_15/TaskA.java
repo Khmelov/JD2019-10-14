f2b8542144e5ca8d8d171c44ad39e0d57c68ff50
Yeroshevich Valeriy <pybin2001@gmail.com>
2019-12-06 12:46:27 +0300

    jd15

--- src/by/it/zimina/jd01_15/TaskA.java
+++ src/by/it/zimina/jd01_15/TaskA.java
@@ -1,30 +1,36 @@
 package by.it.zimina.jd01_15;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class TaskA {
+
     private static String dir(Class<?> cl){
         String path=System.getProperty("user.dir")+ File.separator+"src"+File.separator;
         String clDir=cl.getName().replace(cl.getSimpleName(),"").replace(".",File.separator);
         return path+clDir;
     }
 
-    private static String generateMatrix(){
-        int matrix[][] = new int[6][4];
-        String res = "";
+    private static int[][] generateMatrix(){
+        int matrix[][]= new int[6][4];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 matrix[i][j]=(int)(Math.random() * 30 - 15);
-                res = res + matrix[i][j] + ", ";
             }
-            res=res+"\n";
         }
-        return res;
+        return matrix;
     }
 
-    private static void writeMatrix(){
+    private static void writeMatrix(int[][] matrix){
         try {
-            BufferedWriter file=new BufferedWriter(new FileWriter(dir(TaskA.class) + "martix.txt"));
+            PrintWriter printWriter=new PrintWriter(dir(TaskA.class) + "martix.txt");
+            for (int[] row : matrix) {
+                printWriter.println(Arrays.toString(row)
+                        .replace(",","")
+                        .replace("[","")
+                        .replace("]",""));
+            }
+            printWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -32,8 +38,14 @@ public class TaskA {
     }
 
     public static void main(String[] args) {
-        System.out.println();
-        writeMatrix();
+        int[][] matrix = generateMatrix();
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row)
+                    .replace(",","")
+                    .replace("[","")
+                    .replace("]",""));
+        }
+        writeMatrix(matrix);
 
         }
 
