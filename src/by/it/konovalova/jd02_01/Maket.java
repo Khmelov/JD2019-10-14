963489d21e705bd4cced2639554c072d7d298531
proZao4nik <kkv2185075@gmail.com>
2019-12-09 14:32:44 +0200

    JD02_01 I put the goods in class Goods

--- src/by/it/konovalova/jd02_01/Maket.java
+++ src/by/it/konovalova/jd02_01/Maket.java
@@ -10,23 +10,12 @@ package by.it.konovalova.jd02_01;
 
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Maket {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Market opened");
         List<Buyer> buyerList = new ArrayList<>();
-
-        Map<String, Integer> goods = new HashMap<>();
-        Goods.goods.put("banana", 3);
-        Goods.goods.put("strawberry", 14);
-        Goods.goods.put("pineapple", 8);
-        Goods.goods.put("orange", 6);
-        Goods.goods.put("mango", 7);
-        Goods.goods.put("watermelon", 9);
-        Goods.goods.put("coconut", 5);
 
         for (int i = 0; i < 120; i++) {
             int currentCount = Helper.random(2);
