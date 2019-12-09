48e8b3db3234f6191714723a704cc22aa870527e
zhenyabodukhin <zhenyabodukhin@mail.ru>
2019-12-09 19:22:16 +0300

    jd02_03 A,B completed(working properly)

--- src/by/it/bodukhin/jd02_03/Cashier.java
+++ src/by/it/bodukhin/jd02_03/Cashier.java
@@ -2,6 +2,8 @@ package by.it.bodukhin.jd02_03;
 
 public class Cashier implements Runnable{
 
+    private StringBuilder check = new StringBuilder();
+
     private String name;
 
     public Cashier(int number){
@@ -20,13 +22,23 @@ public class Cashier implements Runnable{
         while ((b= QueueBuyer.extractBuyer())!=null) {
             System.out.println(this + " service " + b);
             Helper.sleep(Helper.random(2000, 5000));
-            System.out.println(b+" bought "+b.goodsInBacket);
-            System.out.println(b + " total price " + b.getTotalPrice()+"$");
-            System.out.println(this + " finished service for " + b);
+            check.append(b).append(" bought ").append(String.join(", ",b.goodsInBacket))
+                    .append("\n").append(b).append(" total price ")
+                    .append(b.getTotalPrice()).append("$")
+                    .append("\n").append(this).append(" finished service for ")
+                    .append(b).append("\n");
+            try {
+                Dispatcher.semPrintToConsole.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print(check);
+            Dispatcher.semPrintToConsole.release();
             synchronized (b) {
                 b.notify();
             }
         }
         System.out.println(this+" cashier closed");
+        Dispatcher.numberOfCashier.getAndDecrement();
     }
 }
