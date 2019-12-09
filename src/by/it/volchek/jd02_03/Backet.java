66214a2698d47ca0be6be419c2faec83bd890e70
yegor-volchek <yegor_volchek@mail.ru>
2019-12-09 14:37:40 +0300

    02_01 AB 02_02 AB 02_03 A

---
+++ src/by/it/volchek/jd02_03/Backet.java
@@ -0,0 +1,57 @@
+package by.it.volchek.jd02_03;
+
+public class Backet {
+
+    public static void backetPut(String name) {
+        int xTimes = Helper.random(1,5);
+        int check = 0;
+        StringBuffer sb = new StringBuffer();
+        sb.append(name + ": ");
+        for (int i = 0; i < xTimes; i++) {
+            int y = Helper.random(0,9);
+            switch (y) {
+                    case 0:
+                        sb.append("(cheese, 2)");
+                    check += 2;
+                    break;
+                    case 1:
+                        sb.append("(milk, 1)");
+                        check += 1;
+                    break;
+                    case 2:
+                        sb.append("(bread, 1)");
+                        check += 1;
+                    break;
+                    case 3:
+                        sb.append("(water, 1)");
+                        check += 1;
+                    break;
+                    case 4:
+                        sb.append("(mayo, 2)");
+                        check += 2;
+                    break;
+                    case 5:
+                        sb.append("(vodka, 7)");
+                        check += 7;
+                    break;
+                    case 6:
+                        sb.append("(ham, 3)");
+                        check += 3;
+                    break;
+                    case 7:
+                        sb.append("(chicken, 6)");
+                        check += 6;
+                    break;
+                    case 8:
+                        sb.append("(cola, 2)");
+                        check += 2;
+                    break;
+                default:
+                    check += 0;
+                    break;
+            }
+        }
+        sb.append("\n" +name + " check = " + check);
+        System.out.println(sb);
+    }
+}
---
+++ src/by/it/volchek/jd02_03/IUseBacket.java
@@ -0,0 +1,6 @@
+package by.it.volchek.jd02_03;
+
+public interface IUseBacket {
+        void takeBacket();
+        void putGoodsToBacket();
+}
