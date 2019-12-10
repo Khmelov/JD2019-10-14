865b0ccc52aaf06dc1f3fdc978b5e582309430fc
zhenyabodukhin <zhenyabodukhin@mail.ru>
2019-12-10 22:51:13 +0300

    jd02_04 A,B(без скобок),C completed

--- src/by/it/bodukhin/calc/Parser.java
+++ src/by/it/bodukhin/calc/Parser.java
@@ -1,37 +1,75 @@
 package by.it.bodukhin.calc;
 
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class Parser {
-    Var evaluate(String expression) throws CalcException {
-        String[] part = expression.split(Patterns.OPERATION, 2);
-        Var right = Var.createVar(part[1]);
-        if(expression.contains("=")) {
-           return Var.saveVar(part[0], right);
+
+    private Map<String, Integer> priority = new HashMap<String,Integer>(){
+        {
+            this.put("=",0);
+            this.put("+",1);
+            this.put("-",1);
+            this.put("*",2);
+            this.put("/",2);
         }
+    };
 
-        Var left = Var.createVar(part[0]);
-
-        if(left!=null && right!=null){
-            Matcher matcher = Pattern
-                    .compile(Patterns.OPERATION)
-                    .matcher(expression);
-            if(matcher.find()){
-                String operation = matcher.group();
-                switch (operation){
-                    case "+":
-                        return left.add(right);
-                    case "-":
-                        return left.sub(right);
-                    case "*":
-                        return left.mul(right);
-                    case "/":
-                        return left.div(right);
-                    default:
-                }
+    private int getIndex(List<String> operators) {
+        int index = -1;
+        int prior = -1;
+        for (int i = 0; i < operators.size(); i++) {
+            String op = operators.get(i);
+            if(priority.get(op)>prior) {
+                prior = priority.get(op);
+                index = i;
             }
         }
-        throw new CalcException("Что-то странное происходит");
+        return index;
+    }
+
+    private Var oneOperation(String strLeft, String operation, String strRight) throws CalcException {
+        Var right = Var.createVar(strRight);
+        if(operation.equals("=")) {
+            Var.saveVar(strLeft, right);
+            return right;
+        }
+        Var left = Var.createVar(strLeft);
+        if(left != null && right !=null) {
+            switch (operation) {
+                case "+":
+                    return left.add(right);
+                case "-":
+                    return left.sub(right);
+                case "*":
+                    return left.mul(right);
+                case "/":
+                    return left.div(right);
+            }
+        }
+        throw new CalcException("Что-то невероятное происходит");
+    }
+
+    Var evaluate(String expression) throws CalcException {
+        String[] part = expression.split(Patterns.OPERATION);
+        if(part.length == 1){
+            return Var.createVar(expression);
+        }
+        List<String> operands = new ArrayList<>(Arrays.asList(part));
+        List<String> operators = new ArrayList<>();
+        Matcher matcher = Pattern.compile(Patterns.OPERATION).matcher(expression);
+        while (matcher.find()){
+            operators.add(matcher.group());
+        }
+        while (operators.size() > 0){
+            int index = getIndex(operators);
+            String left = operands.remove(index);
+            String right = operands.remove(index);
+            String op = operators.remove(index);
+            Var result = oneOperation(left, op, right);
+            operands.add(index, result.toString());
+        }
+        return Var.createVar(operands.get(0));
     }
 }
