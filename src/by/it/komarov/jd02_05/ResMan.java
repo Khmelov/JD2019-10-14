e1cc01d278287bba9e6aed7754c10e36ec4681a1
AntonKomarov <anton.komaro116@gmail.com>
2019-12-16 01:15:24 +0300

    jd02_05

---
+++ src/by/it/komarov/jd02_05/ResMan.java
@@ -0,0 +1,26 @@
+package by.it.komarov.jd02_05;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public enum ResMan {
+    INSTANCE;
+
+    private String path="by.it.komarov.jd02_05.lang.translate";
+
+    private ResourceBundle rb;
+
+    private Locale locale;
+
+    ResMan() {
+        setLocale(Locale.getDefault());
+    }
+
+    void setLocale(Locale locale){
+        this.locale=locale;
+        rb=ResourceBundle.getBundle(path,this.locale);
+    }
+
+    String get(String key) { return rb.getString(key); }
+
+}
