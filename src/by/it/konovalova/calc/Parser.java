e6413e1e586ffdfd33de08663afe1dc4cc58ef0f
proZao4nik <kkv2185075@gmail.com>
2019-12-16 00:40:41 +0200

    JD02_02 - JD02_06, calc

--- src/by/it/konovalova/calc/Parser.java
+++ src/by/it/konovalova/calc/Parser.java
@@ -1,38 +1,81 @@
 package by.it.konovalova.calc;
 
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class Parser {
-    Var calc(String expression) throws CalcException{
-        String[] operand = expression.split(Patterns.OPERATION, 2);
-        Var two = Var.createVar(operand[1]);
-        if(expression.contains("=")){
-            return Var.saveVar(operand[0], two);
+
+    private Map<String, Integer> priority = new HashMap<String, Integer>() {
+        {
+            this.put("=", 0);
+            this.put("+", 1);
+            this.put("-", 1);
+            this.put("*", 2);
+            this.put("/", 2);
         }
-//if(expression.equals("printvar")){
-//    return Scalar.createVar(operand[0]);
-//}
+    };
 
-        Var one = Var.createVar(operand[0]);
-        if (one != null || two != null) {
-            Matcher matcher = Pattern.compile(Patterns.OPERATION).matcher(expression);
-
-            if (matcher.find()) {
-                String operation = matcher.group();
-                switch (operation) {
-                    case "+":
-                        return one.add(two);
-                    case "-":
-                        return one.sub(two);
-                    case "*":
-                        return one.mul(two);
-                    case "/":
-                        return one.div(two);
-
-                }
+    private int getIndex(List<String> operators) {
+        int index = -1;
+        int prior = -1;
+        for (int i = 0; i < operators.size(); i++) {
+            String op = operators.get(i);
+            if (priority.get(op) > prior) {
+                prior = priority.get(op);
+                index = i;
             }
         }
-        return null;
+        return index;
     }
+
+
+    private Var oneOperation(String strLeft, String operation, String strRight) throws CalcException {
+        Var right = Var.createVar(strRight);
+        if (operation.equals("=")) {
+            Var.set(strLeft, right);
+            return right;
+        }
+
+        Var left = Var.createVar(strLeft);
+        if (left != null && right != null) {
+            switch (operation) {
+                case "+":
+                    return left.add(right);
+                case "-":
+                    return left.sub(right);
+                case "*":
+                    return left.mul(right);
+                case "/":
+                    return left.div(right);
+            }
+
+        }
+        throw new CalcException("Что то странное происходит!!!!");
+    }
+
+
+    Var evaluate(String expression) throws CalcException {
+        String[] part = expression.split(Patterns.OPERATION);
+        if (part.length == 1) {
+            return Var.createVar(expression);
+        }
+        List<String> operands = new ArrayList<>(Arrays.asList(part));
+        List<String> operators = new ArrayList<>();
+        Matcher matcher = Pattern.compile(Patterns.OPERATION).matcher(expression);
+        while (matcher.find()) {
+            operators.add(matcher.group());
+        }
+
+        while (operators.size() > 0) {
+            int index = getIndex(operators);
+            String left = operands.remove(index);
+            String rigth = operands.remove(index);
+            String op = operators.remove(index);
+            Var result = oneOperation(left, op, rigth);
+            operands.add(index, result.toString());
+        }
+        return Var.createVar(operands.get(0));
+    }
+
 }
---
+++ src/by/it/konovalova/jd02_03/Goods.java
@@ -0,0 +1,18 @@
+package by.it.konovalova.jd02_03;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Goods {
+
+    static Map<String, Integer> goods = new HashMap<>();
+    static{
+        goods.put("banana",3);
+        goods.put("strawberry",14);
+        goods.put("pineapple",8);
+        goods.put("orange",6);
+        goods.put("mango",7);
+        goods.put("watermelon",9);
+        goods.put("coconut",5);};
+
+}
